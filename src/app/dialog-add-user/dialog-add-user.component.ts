17afa84b74ea500d714f3f5fb8a90b1b82a357b8
Woga65 <woga@mytsa.de>
2022-08-02 20:51:43 +0200

    moved localization to lang.service

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -3,6 +3,7 @@ import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 import { UserService } from '../user.service';
+import { LangService } from '../lang.service';
 
 
 @Component({
@@ -23,6 +24,7 @@ export class DialogAddUserComponent implements OnInit {
 
   constructor(
     public userService: UserService,
+    public langService: LangService,
     public dialogRef: MatDialogRef<DialogAddUserComponent>,
     private _adapter: DateAdapter<any>,
     @Inject(MAT_DATE_LOCALE) private _locale: string,
@@ -74,77 +76,14 @@ export class DialogAddUserComponent implements OnInit {
     this.dialogRef.close(true);
   }
 
-
   switchLang() {
     this.currentLang = (this.currentLang + 1) % this.languages.length;
     this._locale = this.languages[this.currentLang];
     this._adapter.setLocale(this._locale);
   }
 
-  getLocalFormat(): any {
-    switch (this._locale) {
-      case 'ja-JP':
-        return this.jpFormat();
-      case 'de':
-      case 'de-at':
-      case 'de-ch':
-        return this.deFormat();
-      case 'en-gb':
-      case 'fr':
-        return this.gbFormat();
-      case 'en-US':
-        return this.usFormat();
-    }
-    return this.defaultFormat();
-  }
-
-  deFormat(): any {
-    return {
-      table: { title: 'Nutzerliste', fullName: 'Name', eMail: 'E-Mail', address: 'Adresse' },
-      form: { title: 'Nutzer hinzufügen', cancel: 'abbrechen', save: 'speichern', lang: 'Deutsch (DE)' },
-      update: { title: 'Nutzer bearbeiten', delete: 'löschen' },
-      firstName: { text: 'Vorname', placeholder: 'Wolfgang' },
-      lastName: { text: 'Familienname', placeholder: 'Siebert' },
-      birthDate: { text: 'Geburtsdatum', hint: 'DD.MM.YYYY' },
-      eMail: { text: 'E-Mail-Adresse', placeholder: 'woga@web.de' },
-      street: { text: 'Straße + Haus', placeholder: 'Annastraße 16' },
-      zipCode: { text: 'PLZ', placeholder: '34117' },
-      town: { text: 'Ort', placeholder: 'Kassel' },
-    };
-  }
-
-  usFormat(): any {
-    return {
-      table: {}, form: { lang: 'English (US)' }, update: {}, firstName: {}, 
-      lastName: {}, birthDate: { hint: 'MM/DD/YYYY' }, eMail: {},
-      street: {}, zipCode: {}, town: {},
-    };
-  }
-
-  gbFormat(): any {
-    return {
-      table: {}, form: { lang: 'English (GB)' }, update: {}, firstName: { placeholder: 'John' }, 
-      lastName: { placeholder: 'Smith' }, birthDate: { hint: 'DD/MM/YYYY' }, 
-      eMail: { placeholder: 'john@smith.co.uk' }, street: { text: 'House No + Street',
-      placeholder: '85 Fleet street' }, zipCode: { text: 'Postcode', placeholder: 'EC4Y 1AE' },
-      town: { placeholder: 'London' },
-    };
-  }
-
-  jpFormat(): any {
-    return {
-      table: {}, form: { lang: 'English (JP)' }, update: {}, firstName: {},
-      lastName: {}, birthDate: { hint: 'YYYY/MM/DD' }, eMail: {},
-      street: {}, zipCode: {}, town: {},
-    }
-  };
-
-  defaultFormat(): any {
-    return {
-      table: {}, form: { lang: 'English (US)' }, update: {},
-      firstName: {}, lastName: {}, birthDate: { hint: '' },
-      eMail: {}, street: {}, zipCode: {}, town: {},
-    };
+  localize() {
+    return this.langService.getLocalFormat(this._locale);
   }
 
 }
